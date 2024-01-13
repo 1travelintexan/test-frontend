31474468814ba490751d174bc1ddc6da212f9090
Joshua <78570527+1travelintexan@users.noreply.github.com>
2024-01-13 16:08:49 +0100

    todo full stack

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { UserContextWrapper } from "./contexts/UserContext.jsx";
+import { ToDoWrapper } from "./contexts/TodoContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <UserContextWrapper>
+        <ToDoWrapper>
+          <App />
+        </ToDoWrapper>
+      </UserContextWrapper>
+    </BrowserRouter>
+  </React.StrictMode>
+);
